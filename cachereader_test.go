a07ecfde57a09f1d2c8eb69293a45f5c2a000bc5
sfomuseumbot <sfomuseumbot@localhost>
2022-11-18 12:05:37 -0800

    add docs, tests

---
+++ cachereader_test.go
@@ -0,0 +1,74 @@
+package cachereader
+
+import (
+	"context"
+	"fmt"
+	"github.com/whosonfirst/go-reader"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestCacheReader(t *testing.T) {
+
+	ctx := context.Background()
+
+	rel_path := "./fixtures"
+
+	abs_path, err := filepath.Abs(rel_path)
+
+	if err != nil {
+		t.Fatalf("Failed to derive absolute path, %v", err)
+	}
+
+	reader_uri := fmt.Sprintf("fs://%s", abs_path)
+
+	cache_uri := "gocache://"
+
+	cr_uri := fmt.Sprintf("cachereader://?reader=%s&cache=%s", reader_uri, cache_uri)
+
+	r, err := reader.NewReader(ctx, cr_uri)
+
+	if err != nil {
+		t.Fatalf("Failed to create new cachereader, %v", err)
+	}
+
+	to_test := []string{
+		"101736545.geojson",
+	}
+
+	for i := 0; i < 2; i++ {
+
+		for _, path := range to_test {
+
+			fh, err := r.Read(ctx, path)
+
+			if err != nil {
+				t.Fatalf("Failed to read %s, %v", path, err)
+			}
+
+			defer fh.Close()
+
+			_, err = io.Copy(ioutil.Discard, fh)
+
+			if err != nil {
+				t.Fatalf("Failed to copy %s, %v", path, err)
+			}
+
+			v, _ := GetLastRead(r, path)
+
+			switch i {
+			case 0:
+				if v != CacheMiss {
+					t.Fatalf("Expected cache miss on first read of %s", path)
+				}
+			default:
+				if v != CacheHit {
+					t.Fatalf("Expected cache hit after first read of %s", path)
+				}
+			}
+		}
+
+	}
+}
---
+++ doc.go
@@ -0,0 +1,2 @@
+// Package cachereader implements the `whosonfirst/go-reader` interface for reading documents with an internal caching layer.
+package cachereader
